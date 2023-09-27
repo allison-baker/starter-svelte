6f7b792c9f4b877a80557abe85b0c27e328e081c
Al Baker <baker.allison.m@gmail.com>
2023-09-27 15:38:03 -0600

    lifecycle add pokemon

--- src/routes/tutorial/lifecycle/+page.svelte
+++ src/routes/tutorial/lifecycle/+page.svelte
@@ -1,4 +1,24 @@
 <script lang="ts">
-    import { getModalStore } from '@skeletonlabs/skeleton';
-	import type { ModalSettings } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+
+	let pokeImg: string;
+	let pokeName: string;
+
+	onMount(async () => {
+		let randomPokeID = Math.floor(Math.random() * 100) + 1;
+		const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokeID}`);
+		const pokemon = await response.json();
+		pokeImg = pokemon.sprites?.other?.['official-artwork']?.front_default;
+		pokeName = pokemon.name;
+	});
 </script>
+
+<h1 class="m-2 text-2xl">Lifecycle</h1>
+<div class="flex flex-col justify-center items-center">
+	{#if pokeImg}
+		<img src={pokeImg} alt={pokeName} class="h-96 w-96" />
+        <p class="font-bold text-white text-lg bg-secondary-500 rounded-md w-96 text-center py-2">{pokeName.toUpperCase()}</p>
+	{:else}
+		<p>Loading...</p>
+	{/if}
+</div>
