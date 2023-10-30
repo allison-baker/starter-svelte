e42cc81de57d2ade19af8e9411207793841b0212
Al Baker <baker.allison.m@gmail.com>
2023-10-30 16:02:10 -0600

    try view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,22 @@
 
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+
+	import { onNavigate } from '$app/navigation';
+
+	import { browser } from '$app/environment';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
+
 	storePopup.set({
 		computePosition,
 		autoUpdate,
@@ -18,8 +34,6 @@
 		flip,
 		arrow
 	});
-
-	import { browser } from '$app/environment';
 
 	let lightSrc = '/images/personal_logo225.png';
 	let darkSrc = '/images/personal_logo_transparent.png';
@@ -45,15 +59,12 @@
 				<strong class="text-3xl">Al Baker</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<ThemeMenu on:click={() => isDarkMode = !isDarkMode}/>
+				<ThemeMenu on:click={() => (isDarkMode = !isDarkMode)} />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
-		<div
-			id="sidebar-left"
-			class="hidden lg:block bg-tertiary-200 dark:bg-tertiary-900 h-full w-32"
-		>
+		<div id="sidebar-left" class="hidden lg:block bg-tertiary-200 dark:bg-tertiary-900 h-full w-32" style="view-transition-name: sideBar;">
 			<a
 				href="/"
 				class="block p-2 hover:font-bold hover:bg-tertiary-300 dark:hover:bg-tertiary-800 py-4"
@@ -83,3 +94,39 @@
 	</svelte:fragment>
 	<slot />
 </AppShell>
+
+<style>
+	@keyframes fade-in {
+		from {
+			opacity: 0;
+		}
+	}
+
+	@keyframes fade-out {
+		to {
+			opacity: 0;
+		}
+	}
+
+	@keyframes slide-from-right {
+		from {
+			transform: translateX(30px);
+		}
+	}
+
+	@keyframes slide-to-left {
+		to {
+			transform: translateX(-30px);
+		}
+	}
+
+	:root::view-transition-old(root) {
+		animation: 90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
+	}
+
+	:root::view-transition-new(root) {
+		animation: 210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
+	}
+</style>
