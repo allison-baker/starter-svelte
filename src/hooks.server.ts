9c57fed14236d3b858b1fad02547cfe4906da3b4
Al Baker <baker.allison.m@gmail.com>
2023-11-13 14:57:01 -0700

    update auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,17 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
-import Google from '@auth/core/providers/google';
-import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
+import { getEnvironmentData } from 'worker_threads';
+//import Google from '@auth/core/providers/google';
+
+let githubConfig = {clientId: process.env.GITHUB_PROD_ID, clientSecret: process.env.GITHUB_PROD_SECRET}
+
+if (getEnvironmentData.name === "development") {
+	githubConfig = {clientId: process.env.GITHUB_DEV_ID, clientSecret: process.env.GITHUB_DEV_SECRET}
+}
 
 export const handle = SvelteKitAuth({
 	providers: [
-		GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
-		Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
+		GitHub(githubConfig),
+		//Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
 	]
 });
