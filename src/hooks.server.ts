4c79ed347987bb7e19162f875217b2ce2c579126
Al Baker <baker.allison.m@gmail.com>
2023-11-15 16:33:51 -0700

    add google auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,18 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
-//import Google from '@auth/core/providers/google';
+import Google from '@auth/core/providers/google';
 
 let githubConfig = {clientId: process.env.GITHUB_PROD_ID, clientSecret: process.env.GITHUB_PROD_SECRET}
+let googleConfig = {clientId: process.env.GOOGLE_PROD_ID, clientSecret: process.env.GOOGLE_PROD_SECRET}
 
 if (process.env.NODE_ENV === "development") {
 	githubConfig = {clientId: process.env.GITHUB_DEV_ID, clientSecret: process.env.GITHUB_DEV_SECRET}
+	googleConfig = {clientId: process.env.GOOGLE_DEV_ID, clientSecret: process.env.GOOGLE_DEV_SECRET}
 }
 
 export const handle = SvelteKitAuth({
 	providers: [
 		GitHub(githubConfig),
-		//Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
+		Google(googleConfig)
 	]
 });
