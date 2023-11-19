87129c02574e5d840bd0b11f8fe09682f89641cb
Al Baker <baker.allison.m@gmail.com>
2023-11-18 21:37:47 -0700

    add code blocks to motion and transitions

--- src/routes/advancedTutorial/motion/+page.svelte
+++ src/routes/advancedTutorial/motion/+page.svelte
@@ -3,6 +3,17 @@
 	import { cubicOut } from 'svelte/easing';
 	import { ProgressBar } from '@skeletonlabs/skeleton';
 
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import hljs from 'highlight.js/lib/core';
+	import xml from 'highlight.js/lib/languages/xml';
+	import typescript from 'highlight.js/lib/languages/typescript';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+
+	hljs.registerLanguage('xml', xml);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
 	const progress = tweened(0, {
 		duration: 400,
 		easing: cubicOut
@@ -31,24 +42,62 @@
 		height="h-4"
 	/>
 
-	<button on:click={() => progress.set(0)} class="btn variant-filled-secondary my-2">
-		0%
-	</button>
-
-	<button on:click={() => progress.set(25)} class="btn variant-filled-secondary my-2"> 25% </button>
-
-	<button on:click={() => progress.set(50)} class="btn variant-filled-secondary my-2"> 50% </button>
-
-	<button on:click={() => progress.set(75)} class="btn variant-filled-secondary my-2"> 75% </button>
-
-	<button on:click={() => progress.set(100)} class="btn variant-filled-secondary my-2">
-		100%
-	</button>
+	<button on:click={() => progress.set(0)} class="btn variant-filled-secondary my-2">0%</button>
+	<button on:click={() => progress.set(25)} class="btn variant-filled-secondary my-2">25%</button>
+	<button on:click={() => progress.set(50)} class="btn variant-filled-secondary my-2">50%</button>
+	<button on:click={() => progress.set(75)} class="btn variant-filled-secondary my-2">75%</button>
+	<button on:click={() => progress.set(100)} class="btn variant-filled-secondary my-2">100%</button>
 </div>
 
 <p class="m-4 text-sm font-bold">
 	This section uses tweening to animate changes made to the DOM at the click of a button.
 </p>
+
+<CodeBlock
+	language="xml"
+	code={`<ProgressBar
+	label="Progress Bar"
+	value={$progress}
+	max={100}
+	class="my-2"
+	meter="bg-success-500 dark:bg-success-400"
+	track="bg-surface-200 dark:bg-surface-400"
+	height="h-4"
+/>
+
+<button on:click={() => progress.set(0)} class="btn variant-filled-secondary my-2">
+	0%
+</button>
+<button on:click={() => progress.set(25)} class="btn variant-filled-secondary my-2">
+	25%
+</button>
+<button on:click={() => progress.set(50)} class="btn variant-filled-secondary my-2">
+	50%
+</button>
+<button on:click={() => progress.set(75)} class="btn variant-filled-secondary my-2">
+	75%
+</button>
+<button on:click={() => progress.set(100)} class="btn variant-filled-secondary my-2">
+	100%
+</button>`}
+	class="m-4 w-2/3"
+/>
+
+<p class="m-4 text-sm font-bold">
+	This is the typescript used to control the animation of the Progress Bar.
+</p>
+
+<CodeBlock
+	language="typescript"
+	code={`import { tweened } from 'svelte/motion';
+import { cubicOut } from 'svelte/easing';
+
+const progress = tweened(0, {
+	duration: 400,
+	easing: cubicOut
+});`}
+	class="m-4 w-2/3"
+/>
 
 <hr class="border-2" />
 
@@ -100,3 +149,50 @@
 <p class="m-4 text-sm font-bold">
 	This section uses spring to change the damping and stiffness of an SVG.
 </p>
+
+<CodeBlock
+	language="xml"
+	code={`<div class="w-[200px] select-none">
+	<label>
+		<h3>stiffness ({coords.stiffness})</h3>
+		<input bind:value={coords.stiffness} type="range" min="0.01" max="1" step="0.01" class="w-full" />
+	</label>
+
+	<label>
+		<h3>damping ({coords.damping})</h3>
+		<input bind:value={coords.damping} type="range" min="0.01" max="1" step="0.01" class="w-full" />
+	</label>
+</div>
+
+<svg
+	on:mousemove={(e) => {
+		const { left, top, width, height } = e.currentTarget.getBoundingClientRect();
+		coords.set({ x: e.clientX - left, y: e.clientY - top });
+	}}
+	on:mousedown={() => size.set(30)}
+	on:mouseup={() => size.set(10)}
+	class="w-full h-128 border-2 border-tertiary-400 rounded-md ml-2"
+>
+	<circle cx={$coords.x} cy={$coords.y} r={$size} fill="#663363" />
+</svg>`}
+	class="m-4 w-2/3"
+/>
+
+<p class="m-4 text-sm font-bold">
+	This is the typescript used to set the default stiffness and damping values for the spring function.
+</p>
+
+<CodeBlock
+	language="typescript"
+	code={`import { spring } from 'svelte/motion';
+
+let coords = spring(
+	{ x: 50, y: 50 },
+	{
+		stiffness: 0.1,
+		damping: 0.25
+	}
+);
+let size = spring(10);`}
+	class="m-4 w-2/3"
+/>
--- src/routes/advancedTutorial/transitions/+page.svelte
+++ src/routes/advancedTutorial/transitions/+page.svelte
@@ -4,6 +4,15 @@
 	import { typewriter } from '$lib/utils/transition';
 	import { messages } from '$lib/utils/loading-messages';
 
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import hljs from 'highlight.js/lib/core';
+	import xml from 'highlight.js/lib/languages/xml';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+
+	hljs.registerLanguage('xml', xml);
+	storeHighlightJs.set(hljs);
+
 	let visible = true;
 	let visible2 = true;
 	let visible3 = true;
@@ -49,6 +58,23 @@
 	This checkbox uses the Svelte fade transition to show and hide an image.
 </p>
 
+<CodeBlock
+	language="xml"
+	code={`<label>
+	<input type="checkbox" bind:checked={visible} /> Fade Me
+</label>
+
+{#if visible}
+	<img
+		src="https://images.unsplash.com/photo-1478098711619-5ab0b478d6e6?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+		transition:fade
+		class="h-48 w-auto"
+		alt="cat"
+	/>
+{/if}`}
+	class="m-4 w-2/3"
+/>
+
 <hr class="border-2" />
 
 <div class="mx-4 my-2">
@@ -76,6 +102,28 @@
 	events to know when the transition starts and ends.
 </p>
 
+<CodeBlock
+	language="xml"
+	code={`<label>
+	<input type="checkbox" bind:checked={visible2} /> Fly Me
+</label>
+
+<p>Status: {status}</p>
+
+{#if visible2}
+	<p
+		transition:fly={{ y: 200, duration: 2000 }}
+		on:introstart={() => (status = 'intro started')}
+		on:outrostart={() => (status = 'outro started')}
+		on:introend={() => (status = 'intro ended')}
+		on:outroend={() => (status = 'outro ended')}
+		>
+			This paragraph flies!
+		</p>
+{/if}`}
+	class="m-4 w-2/3"
+/>
+
 <hr class="border-2" />
 
 <div class="mx-4 my-2">
@@ -91,6 +139,20 @@
 <p class="m-4 text-sm font-bold">
 	The 'in' and 'out' directives can specify different transitions in or out.
 </p>
+
+<CodeBlock
+	language="xml"
+	code={`<label>
+	<input type="checkbox" bind:checked={visible3} /> Fade Me In, Fly Me Out
+</label>
+
+{#if visible3}
+	<p in:fade out:fly={{ x: 200, duration: 2000 }}>
+		I can do different transitions in and out!
+	</p>
+{/if}`}
+	class="m-4 w-2/3"
+/>
 
 <hr class="border-2" />
 
@@ -108,6 +170,19 @@
 <p class="m-4 text-sm font-bold">
 	This checkbox uses a custom JS transition to create a typewriter effect.
 </p>
+
+<CodeBlock
+	language="xml"
+	code={`<label>
+	<input type="checkbox" bind:checked={visible4} />
+	Typewriter Effect
+</label>
+
+{#if visible4}
+	<p transition:typewriter={{ speed: 1 }}>The quick brown fox jumps over the lazy dog</p>
+{/if}`}
+	class="m-4 w-2/3"
+/>
 
 <hr class="border-2" />
 
@@ -134,6 +209,26 @@
 	when the checkbox is toggled.
 </p>
 
+<CodeBlock
+	language="xml"
+	code={`<label>
+	<input type="checkbox" bind:checked={showItems} /> Show List
+</label>
+
+<label>
+	<input type="range" bind:value={i} max="10" />
+</label>
+
+{#if showItems}
+	{#each items.slice(0, i) as item}
+		<div transition:slide|global class="border-t border-tertiary-300 py-2">
+			{item}
+		</div>
+	{/each}
+{/if}`}
+	class="m-4 w-2/3"
+/>
+
 <hr class="border-2" />
 
 <div class="mx-4 my-2">
@@ -147,5 +242,18 @@
 </div>
 
 <p class="m-4 text-sm font-bold">
-	This section uses a key block to destroy and recreate content every time the message changes, as well as the custom typewriter transition to display the message.
+	This section uses a key block to destroy and recreate content every time the message changes, as
+	well as the custom typewriter transition to display the message.
 </p>
+
+<CodeBlock
+	language="xml"
+	code={`<h2 class="text-lg text-primary-500 dark:text-primary-200">loading...</h2>
+
+{#key j}
+	<p in:typewriter={{ speed: 10 }}>
+		{messages[j] || ''}
+	</p>
+{/key}`}
+	class="m-4 w-2/3"
+/>
